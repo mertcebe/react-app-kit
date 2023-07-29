0af43e05d696feb54d6fdf12de1e3209b4b9d257
mertcebe <mr.mertcebe@gmail.com>
2023-07-29 22:29:53 +0300

    updated

--- src/component/HomePage.js
+++ src/component/HomePage.js
@@ -1,17 +1,44 @@
 import React, { useContext } from 'react'
 import { useSelector } from 'react-redux'
 import User from './User';
+import styled from 'styled-components'
+
+
+const Title = styled.h2`
+    background: red;
+    color: #000;
+`;
+
+const MyDiv = styled.div`
+    height: 90vh;
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+    align-items: flex-start;
+`;
+
+const newDiv = styled.div({
+    display: "flex"
+})
 
 const HomePage = () => {
     let users = useSelector((state) => {
         return state.users
     });
     return (
-        <div style={{height: "90vh", display: "flex", flexDirection: "column", flexWrap: "wrap", alignItems: "flex-start" }}>
-            {users.map((user, index) => {
-                return <User key={index} user={user} />
-            })}
-        </div>
+        <>
+            <Title>awd</Title>
+            {/* <div style={{height: "90vh", display: "flex", flexDirection: "column", flexWrap: "wrap", alignItems: "flex-start"}}>
+                {users.map((user, index) => {
+                    return <User key={index} user={user} />
+                })}
+            </div> */}
+            <MyDiv>
+                {users.map((user, index) => {
+                    return <User key={index} user={user} />
+                })}
+            </MyDiv>
+        </>
     )
 }
 
